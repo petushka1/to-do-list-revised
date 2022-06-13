0f3fb7fe43a58c4ad0cb4822ab025018d623be19
petushka1 <nadya@devery.io>
2022-06-13 11:53:09 +0300

    edit boundler forlder

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
      index: './src/index.js',
   },
   devServer: {
-  static: './dist',
+  static: './doc',
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -17,7 +17,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
   ],
    output: {
      filename: 'index.js',
-     path: path.resolve(__dirname, 'dist'),
+     path: path.resolve(__dirname, 'doc'),
      clean: true,
    },
    module: {
